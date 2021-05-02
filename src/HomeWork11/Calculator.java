bd353fe5ec147c987bc1aa7b8af959fe96335018
Yaroslav Pyrikov <yarko92@rambler.ru>
2021-05-02 17:16:08 +0300

    fix задачи "Калькулятор" кидаем детализированный эксепшен

--- src/HomeWork11/Calculator.java
+++ src/HomeWork11/Calculator.java
@@ -9,7 +9,12 @@ public class Calculator {
     BinaryOperator<Integer> plus = (x, y) -> x + y;
     BinaryOperator<Integer> minus = (x, y) -> x - y;
     BinaryOperator<Integer> multiply = (x, y) -> x * y;
-    BinaryOperator<Double> devide = (x, y) -> y == 0 ? Double.valueOf("Нельзя делить на ноль") : x / y;
+    BinaryOperator<Double> devide = (x, y) -> {
+        if (y == 0) {
+            throw new ArithmeticException("Попытка деления на ноль");
+        } else
+            return x / y;
+    };
 
     UnaryOperator<Integer> pow = x -> x * x;
     UnaryOperator<Integer> abs = x -> x > 0 ? x : x * -1;
